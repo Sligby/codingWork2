d23a4732881e16149bba97da858fc40a7d4cd5c9
root <root@LAPTOP-N1AEFKV0.localdomain>
2023-01-11 00:03:05 -0500

    finished both apps

--- flask-greet-calc/calc/app.py
+++ flask-greet-calc/calc/app.py
@@ -1,1 +1,37 @@
-# Put your app in here.
+from flask import Flask, request
+
+# Import the functions from the operations module
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+@app.route('/add')
+def add_route():
+    a = request.args.get('a')
+    b = request.args.get('b')
+    result = add(a, b)
+    return str(result)
+
+@app.route('/sub')
+def sub_route():
+    a = request.args.get('a')
+    b = request.args.get('b')
+    result = sub(a, b)
+    return str(result)
+
+@app.route('/mult')
+def mult_route():
+    a = request.args.get('a')
+    b = request.args.get('b')
+    result = mult(a, b)
+    return str(result)
+
+@app.route('/div')
+def div_route():
+    a = request.args.get('a')
+    b = request.args.get('b')
+    result = div(a, b)
+    return str(result)
+
+if __name__ == '__main__':
+    app.run()
